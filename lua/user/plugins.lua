1408074a6c392201e5fe4eeb6382b98e26c103df
Thing-han, Lim <b029011144@gmail.com>
2022-06-05 23:27:57 +0800

    feat: use Bufferline plugin
    Closes #30

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -65,6 +65,9 @@ return packer.startup(function(use)
 	-- git
 	use("lewis6991/gitsigns.nvim")
 
+    -- bufferline
+    use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
