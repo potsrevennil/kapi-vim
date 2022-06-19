34cefad57cadabeaf4555a87db258455c5553e14
Thing-han, Lim <b029011144@gmail.com>
2022-06-19 23:08:44 +0800

    chore: setup keymaps
    Remove <Space> prefix for keymaps
    Add keymap for toggling diagnostic virtual_text

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,7 +1,11 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 keymap("", "<A-,>", "<C-o>", opts)
 
-keymap("n", "<Space>qf", ":lua vim.diagnostic.setqflist()<CR>", opts)
+keymap("n", "qf", ":lua vim.diagnostic.setqflist()<CR>", opts)
+
+keymap("n", "ff", ":lua vim.lsp.buf.formatting()<CR>", opts)
+
+keymap("n", "dv", ":lua vim.diagnostic.config({virtual_text = not vim.diagnostic.config().virtual_text})<CR>", opts)
