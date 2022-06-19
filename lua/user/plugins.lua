f3a2ff93f114b90cd954bed9f9f23c151cc8d966
Thing-han, Lim <b029011144@gmail.com>
2022-06-19 23:07:48 +0800

    build: load null-ls at startup
    Load null-ls at startup for convenience

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -80,7 +80,6 @@ return packer.startup(function(use)
 
 	use({
 		"jose-elias-alvarez/null-ls.nvim",
-		keys = "<Leader>nl",
 		config = "require 'user.null-ls'",
 		requires = "nvim-lua/plenary.nvim",
 	})
