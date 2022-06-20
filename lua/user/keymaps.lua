b365dcefbc0a041305134dd3ed2d0df7aab5956c
Thing-han, Lim <b029011144@gmail.com>
2022-06-20 12:52:07 +0800

    feat: add keymaps for diagnostics

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -9,3 +9,6 @@ keymap("n", "qf", ":lua vim.diagnostic.setqflist()<CR>", opts)
 keymap("n", "ff", ":lua vim.lsp.buf.formatting()<CR>", opts)
 
 keymap("n", "dv", ":lua vim.diagnostic.config({virtual_text = not vim.diagnostic.config().virtual_text})<CR>", opts)
+keymap("n", "do", ":lua vim.diagnostic.open_float()<CR>", opts)
+keymap("n", "dh", ":lua vim.diagnostic.hide()<CR>", opts)
+keymap("n", "ds", ":lua vim.diagnostic.show()<CR>", opts)
