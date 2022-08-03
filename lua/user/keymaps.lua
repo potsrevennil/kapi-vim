c2b52679ee3b5b91161f977c40a58edbc40e85a1
Thing-han, Lim <b029011144@gmail.com>
2022-08-03 13:51:38 +0800

    feat: update shortcut key mapping

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -4,11 +4,18 @@ local keymap = vim.api.nvim_set_keymap
 
 keymap("", "<A-,>", "<C-o>", opts)
 
-keymap("n", "qf", ":lua vim.diagnostic.setqflist()<CR>", opts)
+keymap("n", "<Leader>ff", ":lua vim.lsp.buf.formatting()<CR>", opts)
 
-keymap("n", "ff", ":lua vim.lsp.buf.formatting()<CR>", opts)
+keymap(
+	"n",
+	"<Leader><Leader>dv",
+	":lua vim.diagnostic.config({virtual_text = not vim.diagnostic.config().virtual_text})<CR>",
+	opts
+)
+keymap("n", "<Leader>do", ":lua vim.diagnostic.open_float()<CR>", opts)
+keymap("n", "<Leader>dh", ":lua vim.diagnostic.hide()<CR>", opts)
+keymap("n", "<Leader>ds", ":lua vim.diagnostic.show()<CR>", opts)
+keymap("n", "<Leader>dl", ":lua vim.diagnostic.setqflist()<CR>", opts)
 
-keymap("n", "dv", ":lua vim.diagnostic.config({virtual_text = not vim.diagnostic.config().virtual_text})<CR>", opts)
-keymap("n", "do", ":lua vim.diagnostic.open_float()<CR>", opts)
-keymap("n", "dh", ":lua vim.diagnostic.hide()<CR>", opts)
-keymap("n", "ds", ":lua vim.diagnostic.show()<CR>", opts)
+keymap("n", "<Leader>qf", ":lua vim.lsp.buf.code_action({only=quickfix})<CR>", opts)
+keymap("n", "<Leader>rf", ":lua vim.lsp.buf.code_action({only=refactor})<CR>", opts)
