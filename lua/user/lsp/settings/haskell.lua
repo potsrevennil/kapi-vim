20f6b3861035f2bfa14c04c1ec03d73b7d9f938c
Thing-han, Lim <b029011144@gmail.com>
2022-08-03 13:50:30 +0800

    feat: add lsp config for haskell

---
+++ lua/user/lsp/settings/haskell.lua
@@ -0,0 +1,13 @@
+return {
+	settings = {
+		formattingProvider = "brittany",
+		plugin = {
+			tactics = {
+				globalOn = true,
+				config = {
+					hole_severity = 4,
+				},
+			},
+		},
+	},
+}
