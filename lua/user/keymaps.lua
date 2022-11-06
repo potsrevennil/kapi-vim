a513ebb48c42e7a1294f0cdc526d0b4692d3033d
Thing-han, Lim <b029011144@gmail.com>
2022-11-06 17:27:18 +0800

    chore: update lsp config according to neovim update

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,10 +1,8 @@
 vim.g.mapleader = " "
 local opts = { noremap = true, silent = true }
-local keymap = vim.api.nvim_set_keymap
+local keymap = vim.keymap.set
 
 keymap("", "<A-,>", "<C-o>", opts)
-
-keymap("n", "<Leader>ff", ":lua vim.lsp.buf.formatting()<CR>", opts)
 
 keymap(
 	"n",
@@ -12,12 +10,15 @@ keymap(
 	":lua vim.diagnostic.config({virtual_text = not vim.diagnostic.config().virtual_text})<CR>",
 	opts
 )
-keymap("n", "<Leader>do", ":lua vim.diagnostic.open_float()<CR>", opts)
-keymap("n", "<Leader>dh", ":lua vim.diagnostic.hide()<CR>", opts)
-keymap("n", "<Leader>ds", ":lua vim.diagnostic.show()<CR>", opts)
-keymap("n", "<Leader>dl", ":lua vim.diagnostic.setqflist()<CR>", opts)
+keymap("n", "<Leader>dh", vim.diagnostic.hide, opts)
+keymap("n", "<Leader>ds", vim.diagnostic.show, opts)
+keymap("n", "<Leader>dl", vim.diagnostic.setqflist, opts)
 
-keymap("n", "<Leader>qf", ":lua vim.lsp.buf.code_action({only=quickfix})<CR>", opts)
-keymap("n", "<Leader>rf", ":lua vim.lsp.buf.code_action({only=refactor})<CR>", opts)
+keymap("n", "<Leader>qf", function()
+	vim.lsp.buf.code_action({ only = quickfix })
+end, opts)
+keymap("n", "<Leader>rf", function()
+	vim.lsp.buf.code_action({ only = refactor })
+end, opts)
 
 keymap("n", "<F2>", ":silent! source ./.session.vim<CR>", opts)
--- lua/user/lsp/config.lua
+++ lua/user/lsp/config.lua
@@ -66,7 +66,7 @@ end
 
 local function lsp_keymaps(bufnr)
 	local function buf_set_keymap(...)
-		vim.api.nvim_buf_set_keymap(bufnr, ...)
+		vim.keymap.set(...)
 	end
 
 	local function buf_set_option(...)
@@ -77,22 +77,35 @@ local function lsp_keymaps(bufnr)
 
 	-- Mappings.
 	local opts = { noremap = true, silent = true }
-	buf_set_keymap("n", "<leader>d", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	buf_set_keymap("n", "<space>.", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	buf_set_keymap("n", "<space>k", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-	buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-	buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-	buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-	buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({border='rounded'})<CR>", opts)
-	buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev({border='rounded'})<CR>", opts)
-	buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next({border='rounded'})<CR>", opts)
-	buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
+	buf_set_keymap("n", "<space>e", vim.diagnostic.open_float, opts)
+	buf_set_keymap("n", "[d", function()
+		vim.diagnostic.goto_prev({ border = "rounded" })
+	end, opts)
+	buf_set_keymap("n", "]d", function()
+		vim.diagnostic.goto_next({ border = "rounded" })
+	end, opts)
+	buf_set_keymap("n", "<space>q", vim.diagnostic.setloclist, opts)
+
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	buf_set_keymap("n", "<leader>d", vim.lsp.buf.declaration, bufopts)
+	buf_set_keymap("n", "<space>.", vim.lsp.buf.definition, bufopts)
+	buf_set_keymap("n", "<space>k", vim.lsp.buf.hover, bufopts)
+	buf_set_keymap("n", "gi", vim.lsp.buf.implementation, bufopts)
+	buf_set_keymap("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	buf_set_keymap("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	buf_set_keymap("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	buf_set_keymap("n", "<space>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	buf_set_keymap("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+	buf_set_keymap("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+	buf_set_keymap("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+	buf_set_keymap("n", "gr", vim.lsp.buf.references, bufopts)
+	buf_set_keymap("n", "<space>f", function()
+		vim.lsp.buf.format({ async = true })
+	end, bufopts)
 end
 
 M.on_attach = function(client, bufnr)
