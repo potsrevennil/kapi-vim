f735abe37f6d623d3b45d73179eb87eff3c7279b
Thing-han, Lim <b029011144@gmail.com>
2023-03-10 14:34:31 +0800

    style: format lua file

--- lua/user/lsp/settings/rust_analyzer.lua
+++ lua/user/lsp/settings/rust_analyzer.lua
@@ -1,29 +1,40 @@
 return {
-	settings = {
-		["rust_analyzer"] = {
-			-- assist = {
-			-- 	importGranularity = "module",
-			-- 	importPrefix = "self",
-			-- },
-			imports = {
-				granularity = {
-					group = "module",
-				},
-				prefix = "self",
-			},
-			cargo = {
-				loadOutDirsFromCheck = true,
-			},
-			procMacro = {
-				enable = true,
-				attributes = {
-					enable = true,
-				},
-				derive = true,
-			},
-			checkOnSave = {
-				command = "clippy",
-			},
-		},
-	},
+    settings = {
+        ['rust_analyzer'] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                    invocationLocation = "root",
+                    invocationStrategy = "once",
+                    overrideCommand = "cargo check --message-format=json",
+                },
+            },
+            procMacro = {
+                enable = true,
+                attributes = {
+                    enable = true,
+                },
+                derive = true,
+            },
+            check = {
+                command = "clippy",
+            },
+            checkOnSave = {
+                command = "clippy",
+            },
+            diagnostics = {
+                enable = true,
+                experimental = {
+                    enable = false,
+                },
+                disabled = { "unresolved-proc-macro" },
+            },
+        },
+    },
 }
