aac8036eb8b74d942a5ff91e26b3913b5838d27b
Thing-han, Lim <b029011144@gmail.com>
2023-03-10 14:32:50 +0800

    feat: use lspconfig instead of mason temporary
    Due to unable to hide rust analyzer diagnostics unresolved method error

--- lua/user/lsp/installer.lua
+++ lua/user/lsp/installer.lua
@@ -6,45 +6,60 @@ function M.setup(opts)
         return
     end
 
-    require("mason").setup({
-        ui = {
-            icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗",
-            },
-        },
-    })
-    require("mason-lspconfig").setup({
-        ensure_installed = {
-            "lua_ls",
-            "bashls",
-            "rust_analyzer",
-            "gopls",
-            "hls",
-            "pylsp",
-            "taplo",
-            "yamlls",
-            "jsonls",
-            "dockerls",
-        },
-    })
+    local servers = {
+        "lua_ls",
+        "bashls",
+        "rust_analyzer",
+        "gopls",
+        "hls",
+        "pylsp",
+        "taplo",
+        "yamlls",
+        "jsonls",
+        "dockerls",
+    }
 
-    require("mason-lspconfig").setup_handlers({
-        function(server_name)
-            if server_name == "sumneko_lua" then
-                local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-                opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-            end
+    for _, server in pairs(servers) do
+        local require_ok, conf_opts = pcall(require, "user.lsp.settings."..server)
+        if require_ok then
+            opts = vim.tbl_deep_extend("force", conf_opts, opts)
+        end
+        lspconfig[server].setup(opts)
+    end
 
-            if server_name == "rust_analyzer" then
-                local rust_opts = require("user.lsp.settings.rust_analyzer")
-                opts = vim.tbl_deep_extend("force", rust_opts, opts)
-            end
-
-            lspconfig[server_name].setup(opts)
-        end,
-    })
+    -- require("mason").setup({
+    --     ui = {
+    --         icons = {
+    --             package_installed = "✓",
+    --             package_pending = "➜",
+    --             package_uninstalled = "✗",
+    --         },
+    --     },
+    -- })
+    -- require("mason-lspconfig").setup({
+    --     ensure_installed = {
+    --         "lua_ls",
+    --         "bashls",
+    --         "rust_analyzer",
+    --         "gopls",
+    --         "hls",
+    --         "pylsp",
+    --         "taplo",
+    --         "yamlls",
+    --         "jsonls",
+    --         "dockerls",
+    --     },
+    -- })
+    --
+    -- require("mason-lspconfig").setup_handlers({
+    --     function(server_name)
+    --         local require_ok, conf_opts = pcall(require, "user.lsp.settings."..server_name)
+    --         if require_ok then
+    --             opts = vim.tbl_deep_extend("force", conf_opts, opts)
+    --         end
+    --         lspconfig[server_name].setup(opts)
+    --     end,
+    -- })
 end
 
 return M
