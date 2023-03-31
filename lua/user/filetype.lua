64337965dff5563413b124d7757a746515471b59
Thing-han, Lim <b029011144@gmail.com>
2023-03-31 15:59:20 +0800

    feat: update sqlite3 filetype

--- lua/user/filetype.lua
+++ lua/user/filetype.lua
@@ -1,14 +1,16 @@
 local status_ok, filetype = pcall(require, "filetype")
 if not status_ok then
-	return
+    return
 end
 
 vim.g.did_load_filetypes = 1
 
 filetype.setup({
-	overrides = {
-		extensions = {
-			sol = "solidity",
-		},
-	},
+    overrides = {
+        extensions = {
+            sol = "solidity",
+            jsonnet = "jsonnet",
+            sqlite3 = "sql",
+        },
+    },
 })
