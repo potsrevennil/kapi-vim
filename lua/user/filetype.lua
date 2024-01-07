11bab3e3762c6fd84afcbdbfbc80217231697462
Thing-han, Lim <b029011144@gmail.com>
2024-01-07 22:01:39 +0800

    refactor: move global config outof user directory and remove mason/packer dependencies
    Remove obsolete files as well

---
+++ lua/user/filetype.lua
@@ -0,0 +1,14 @@
+local status_ok, filetype = pcall(require, "filetype")
+if not status_ok then
+    return
+end
+
+vim.g.did_load_filetypes=1
+
+filetype.setup({
+    overrides = {
+        extensions = {
+            sol = "solidity"
+        }
+    }
+})
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,35 +1,16 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+    return
 end
 
 configs.setup({
-	ensure_installed = {
-		"bash",
-		"gitignore",
-		"go",
-		"gomod",
-		"javascript",
-		"json",
-		"lua",
-		"markdown",
-		"python",
-		"rust",
-		"sql",
-		"tlaplus",
-		"toml",
-		"typescript",
-		"vim",
-		"yaml",
-		"jsonnet",
-	},
-	sync_install = false,
-	autopairs = {
-		enable = true,
-	},
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = { enable = true, disable = { "yaml" } },
+    sync_install = false,
+    autopairs = {
+        enable = true,
+    },
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    indent = { enable = true, disable = { "yaml" } },
 })
