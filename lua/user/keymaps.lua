3ff88a3f7b5467b91b58a027aecd86c83fc55e7b
Thing-han, Lim <15379156+potsrevennil@users.noreply.github.com>
2025-07-21 01:52:15 +0800

    chore: remove deprecated keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -13,12 +13,3 @@ keymap(
 keymap("n", "<Leader>dh", vim.diagnostic.hide, opts)
 keymap("n", "<Leader>ds", vim.diagnostic.show, opts)
 keymap("n", "<Leader>dl", vim.diagnostic.setqflist, opts)
-
-keymap("n", "<Leader>qf", function()
-    vim.lsp.buf.code_action({ only = quickfix })
-end, opts)
-keymap("n", "<Leader>rf", function()
-    vim.lsp.buf.code_action({ only = refactor })
-end, opts)
-
-keymap("n", "<F2>", ":silent! source ./.session.vim<CR>", opts)
